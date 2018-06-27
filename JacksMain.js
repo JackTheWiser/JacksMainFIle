1b1b5071d93a54800dba0979e6430fb2355dbe1a
JackTheWiser <40637591+JackTheWiser@users.noreply.github.com>
2018-06-27 10:28:29 -0500

    Add files via upload

---
+++ JacksMain.js
@@ -0,0 +1,17 @@
+function conosolePrint(){
+    console.log("hello Jon");
+  }
+   function alertPrint(){
+    alert("hello Jon");
+  }
+   function documentPrint(){
+    document.write("Hola john")
+  } function conosolePrint2(){
+    console.log("It's okay bruh, I still love you");
+  }
+   function alertPrint2(){
+    alert("I hate you");
+  }
+   function documentPrint2(){
+    document.write("I'll be your daddy if you want")
+  } 
